3c0797aa53a8b919a684a5f3a4d6ba42d9365918
Feru0110 <mugalde70964@ufide.ac.cr>
2023-07-04 21:22:00 -0600

    cambios

--- Proyecto/src/proyecto/Orden.java
+++ Proyecto/src/proyecto/Orden.java
@@ -4,12 +4,56 @@
  */
 package proyecto;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author feru0
  */
 public class Orden {
     
+    
+     private String ID;
+     private String nombre ; 
+     private int numMesa;
+     
+     
+     public Orden(String ID , String nombre , int numMesa){
+         
+         
+         this.ID = ID;
+         this.nombre = nombre;
+         this.numMesa= numMesa;
+         
+     }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getNumMesa() {
+        return numMesa;
+    }
+
+    public void setNumMesa(int numMesa) {
+        this.numMesa = numMesa;
+    }
+     
+     
+     
+     
     
     
 }
