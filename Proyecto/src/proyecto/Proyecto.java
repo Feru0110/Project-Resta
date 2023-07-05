d3e6196914dbd59eee123d38c6ff620086c75f21
Feru0110 <mugalde70964@ufide.ac.cr>
2023-07-04 21:22:34 -0600

    Update Proyecto.java

--- Proyecto/src/proyecto/Proyecto.java
+++ Proyecto/src/proyecto/Proyecto.java
@@ -15,14 +15,39 @@ public class Proyecto {
 
     public static void main(String[] args) {
         
+        
+        //fer
+        // lo primero que tiene que aparecer en el programa 
+        String botonesPrincipal[] = {"RESTAURANTE", "ORDENES", "MENU", "REPORTES", "SALIR"};
 
-
-        String botones[] = {"RESTAURANTE", "ORDENES", "MENU", "REPORTES", "SALIR"};
-
-        int boton = JOptionPane.showOptionDialog(null, "-----BIENVENIDO A DELICIAS DEL MAR PROFUNDO-----", "OPCIONES?", JOptionPane.DEFAULT_OPTION,
+        int botonPrincipal = JOptionPane.showOptionDialog(null, "-----BIENVENIDO A DELICIAS DEL MAR PROFUNDO-----", "OPCIONES?", JOptionPane.DEFAULT_OPTION,
                 JOptionPane.QUESTION_MESSAGE,
-                null, botones, "RESTAURANTE");
+                null, botonesPrincipal, "RESTAURANTE");
 
+        
+        
+        
+        
+        
+        
+        
+        //fer
+       // menu de la clase orden  
+        int menu;
+        menu = Integer.parseInt(JOptionPane.showInputDialog("-----MENU DE ORDENES-----\n"
+                    + "1 - ABRIR ORDEN\n"
+                    + "2- AGREGAR PLATILLO A LA ORDEN\n"
+                    + "3- CERRAR ORDEN\n"
+                    + "4- SALIR\n"));
+                    
+        
+        //fer
+       // esto es de la orden  
+         String botonesOrden[] = {"AGREGAR OTRO NUMERO", "CANCELAR"};
+
+        int botonOrden = JOptionPane.showOptionDialog(null, "-----ERROR DE NUMERO DE-----", "QUE DESEA?", JOptionPane.DEFAULT_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null, botonesOrden, "CANCELAR");
         
         
     }
